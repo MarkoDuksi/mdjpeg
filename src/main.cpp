4160ded4f51a48146b96eb7222379eb552fe5a01
MarkoDuksi <dookshi@gmail.com>
2023-09-09 22:07:19 +0200

    extend ad hoc visual tests

--- src/main.cpp
+++ src/main.cpp
@@ -85,36 +85,140 @@ int main(int argc, char** argv) {
         auto [buff, size] = read_raw_jpeg_from_file(file_path);
 
         JpegDecoder decoder(buff, size);
-        DirectBlockWriter writer;
+        DirectBlockWriter writer1;
 
-        constexpr uint     x1_blk = 0;
-        constexpr uint     y1_blk = 0;
-        constexpr uint  width_blk = 20;
-        constexpr uint height_blk = 15;
 
-        uint8_t decoded_img[8 * width_blk * 8 * height_blk] {0};
-        if (decoder.decode(decoded_img, x1_blk, y1_blk, x1_blk + width_blk, y1_blk + height_blk, writer)) {
-            for (uint i = 0; i < 8 * height_blk; ++i) {
-                for (uint j = 0; j < 8 * width_blk; ++j) {
-                    std::cout << std::dec << (int)decoded_img[8 * width_blk * i + j] << " ";
-                }
-                std::cout << "\n";
+        constexpr uint     x1_blk1 = 0;
+        constexpr uint     y1_blk1 = 0;
+        constexpr uint  width_blk1 = 20;
+        constexpr uint height_blk1 = 15;
+
+        uint8_t decoded_img1[8 * width_blk1 * 8 * height_blk1] {0};
+        if (!decoder.decode(decoded_img1, x1_blk1, y1_blk1, x1_blk1 + width_blk1, y1_blk1 + height_blk1)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test1.pgm... ";
+            if (!write_pgm("../output/test1.pgm", decoded_img1, 8 * width_blk1, 8 * height_blk1)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
             }
         }
 
-        write_pgm("../output/test1.pgm", decoded_img, 160, 120);
 
-        uint8_t decoded_img2[width_blk * height_blk] {0};
-        if (decoder.dc_decode(decoded_img2, x1_blk, y1_blk, x1_blk + width_blk, y1_blk + height_blk)) {
-            for (uint i = 0; i < height_blk; ++i) {
-                for (uint j = 0; j < width_blk; ++j) {
-                    std::cout << std::dec << (int)decoded_img2[width_blk * i + j] << " ";
-                }
-                std::cout << "\n";
+        constexpr uint     x1_blk2 = 0;
+        constexpr uint     y1_blk2 = 0;
+        constexpr uint  width_blk2 = 20;
+        constexpr uint height_blk2 = 15;
+
+        uint8_t decoded_img2[width_blk2 * height_blk2] {0};
+        if (!decoder.dc_decode(decoded_img2, x1_blk2, y1_blk2, x1_blk2 + width_blk2, y1_blk2 + height_blk2)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test2.pgm... ";
+            if (!write_pgm("../output/test2.pgm", decoded_img2, width_blk2, height_blk2)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
             }
         }
 
-        write_pgm("../output/test2.pgm", decoded_img2, 20, 15);
+
+        constexpr uint     x1_blk3 = 1;
+        constexpr uint     y1_blk3 = 1;
+        constexpr uint  width_blk3 = 7;
+        constexpr uint height_blk3 = 5;
+
+        uint8_t decoded_img3[8 * width_blk3 * 8 * height_blk3] {0};
+        if (!decoder.decode(decoded_img3, x1_blk3, y1_blk3, x1_blk3 + width_blk3, y1_blk3 + height_blk3, writer1)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test3.pgm... ";
+            if (!write_pgm("../output/test3.pgm", decoded_img3, 8 * width_blk3, 8 * height_blk3)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
+            }
+        }
+
+
+        constexpr uint     x1_blk4 = 6;
+        constexpr uint     y1_blk4 = 4;
+        constexpr uint  width_blk4 = 8;
+        constexpr uint height_blk4 = 7;
+
+        uint8_t decoded_img4[8 * width_blk4 * 8 * height_blk4] {0};
+        if (!decoder.decode(decoded_img4, x1_blk4, y1_blk4, x1_blk4 + width_blk4, y1_blk4 + height_blk4, writer1)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test4.pgm... ";
+            if (!write_pgm("../output/test4.pgm", decoded_img4, 8 * width_blk4, 8 * height_blk4)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
+            }
+        }
+
+
+        constexpr uint     x1_blk5 = 0;
+        constexpr uint     y1_blk5 = 0;
+        constexpr uint  width_blk5 = 20;
+        constexpr uint height_blk5 = 15;
+        constexpr uint downscaled_width_px5 = 2 * width_blk5;
+        constexpr uint downscaled_height_px5 = 2 * height_blk5;
+        DownscalingBlockWriter<downscaled_width_px5, downscaled_height_px5> writer2;
+
+        uint8_t decoded_img5[downscaled_width_px5 * downscaled_height_px5] {0};
+        if (!decoder.decode(decoded_img5, x1_blk5, y1_blk5, x1_blk5 + width_blk5, y1_blk5 + height_blk5, writer2)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test5.pgm... ";
+            if (!write_pgm("../output/test5.pgm", decoded_img5, downscaled_width_px5, downscaled_height_px5)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
+            }
+        }
+
+
+        constexpr uint     x1_blk6 = 0;
+        constexpr uint     y1_blk6 = 0;
+        constexpr uint  width_blk6 = 20;
+        constexpr uint height_blk6 = 15;
+        constexpr uint downscaled_width_px6 = 8 * width_blk6;
+        constexpr uint downscaled_height_px6 = 8 * height_blk6;
+        DownscalingBlockWriter<downscaled_width_px6, downscaled_height_px6> writer3;
+
+        uint8_t decoded_img6[downscaled_width_px6 * downscaled_height_px6] {0};
+        if (!decoder.decode(decoded_img6, x1_blk6, y1_blk6, x1_blk6 + width_blk6, y1_blk6 + height_blk6, writer3)) {
+            std::cout << "Decoding failed.\n";
+        }
+        else {
+            std::cout << "Decoding successful.\n";
+            std::cout << "  Writing to ../output/test6.pgm... ";
+            if (!write_pgm("../output/test6.pgm", decoded_img6, downscaled_width_px6, downscaled_height_px6)) {
+                std::cout << "Failed.\n";
+            }
+            else {
+                std::cout << "Successful.\n";
+            }
+        }
+
 
         std::cout << "\nprocessed image: " << file_path << "\n\n";
 
