5b9ec79256cdcc9cb724595ee2f40f0e42141f61
MarkoDuksi <dookshi@gmail.com>
2023-09-19 23:01:34 +0200

    generalize input dims to downscaling test

--- src/tests.h
+++ src/tests.h
@@ -46,26 +46,27 @@ bool run_integral_decoding_tests(std::filesystem::path input_base_dir, const Dim
 bool run_partial_decoding_tests(std::filesystem::path input_base_dir);
 
 // run tests that decode full 800x800 px image frames w/ arbitrary downscaling
-template <uint DST_WIDTH_PX, uint DST_HEIGHT_PX>
+template <uint SRC_WIDTH_PX, uint SRC_HEIGHT_PX, uint DST_WIDTH_PX, uint DST_HEIGHT_PX>
 bool run_downscaling_decoding_tests(std::filesystem::path input_base_dir) {
 
     bool test_passed{true};
 
-    Dimensions src_dims {800, 800};
+    Dimensions src_dims {SRC_WIDTH_PX, SRC_HEIGHT_PX};
     Dimensions dst_dims {DST_WIDTH_PX, DST_HEIGHT_PX};
 
     auto input_files_paths = get_input_img_paths(input_base_dir, src_dims);
 
     for (const auto& file_path : input_files_paths) {
 
-        std::cout << "Running donwscaling decoding test on \"" << file_path.c_str() << "\"... ";
+        std::cout << "Running donwscaling decoding test on \"" << file_path.c_str() << "\""
+                  << " (" << SRC_WIDTH_PX << "x" << SRC_HEIGHT_PX << " -> " << DST_WIDTH_PX << "x" << DST_HEIGHT_PX << ")\n";
 
         auto [buff, size] = read_raw_jpeg_from_file(file_path);
         JpegDecoder decoder(buff, size);
         DownscalingBlockWriter<DST_WIDTH_PX, DST_HEIGHT_PX> writer;
 
         uint8_t decoded_img[DST_WIDTH_PX * DST_HEIGHT_PX] {};
-        if (decoder.decode(decoded_img, 0, 0, 100, 100, writer)) {
+        if (decoder.decode(decoded_img, 0, 0, SRC_WIDTH_PX / 8 , SRC_HEIGHT_PX / 8, writer)) {
 
             std::filesystem::path output_dir = file_path.parent_path() / "downscaled";
             std::filesystem::create_directory(output_dir);
@@ -74,20 +75,17 @@ bool run_downscaling_decoding_tests(std::filesystem::path input_base_dir) {
             if (!write_pgm(output_file_path, decoded_img, dst_dims)) {
 
                 test_passed = false;
-                std::cout << "\n  Writing output at " << DST_WIDTH_PX << "x" << DST_HEIGHT_PX << " px FAILED.\n";
+                std::cout << "\n  Writing output file FAILED.\n";
             }
         }
         else {
 
             test_passed = false;
-            std::cout << "\n  Decoding 800x800 px JPEG w/ rescaling to " << DST_WIDTH_PX << "x" << DST_HEIGHT_PX << " px FAILED.\n";
+            std::cout << "\n  Decoding+downscaling input file FAILED.\n";
         }
 
         delete[] buff;
 
-        if (test_passed) {
-            std::cout << "passed.\n";
-        }
     }
 
     std::cout << "\n";
